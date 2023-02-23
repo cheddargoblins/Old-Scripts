cb46017d7f1afd883408fb0568b9a3c292f916f5
Brandon Bowland <68788218+cheddargoblins@users.noreply.github.com>
2023-02-23 16:35:16 -0600

    Comment and Indentation Fixes

--- Hash Search and Destroy.ps1
+++ Hash Search and Destroy.ps1
@@ -13,26 +13,27 @@ $targets = Get-Content "$home\hostlist.txt"
 
 ForEach ($target in $targets){
 
-Invoke-Command -cn $target -ScriptBlock {
-	$hashs = $null
-	#Checks all possible routes for Windows Disk Drives.
-	[byte]$Alp = [char]'Z'
-	Do {$Alpa = [char]$Alp
-		#Adds hashs of all files recurisely from the root folder. Include keywords in the -Include section to filter on files for Interest.
-		$hashs += Get-Childitem -path "$Alpa`:\" -Include "*1*","*.lnk" -ErrorAction SilentlyContinue -Recurse -Force | Get-FileHash -Algorithm MD5 -ErrorAction SilentlyContinue
-        	#Change Algorithm above from MD5 to another one if desired.
-		$Alp--}
-	Until ($Alp -lt 65)
+	Invoke-Command -cn $target -ScriptBlock {
+		$hashs = $null
+		#Checks all possible routes for Windows Disk Drives.
+		[byte]$Alp = [char]'Z'
+		Do {$Alpa = [char]$Alp
+			#Adds hashs of all files recurisely from the root folder. Include keywords in the -Include section to filter on files for Interest.
+			$hashs += Get-Childitem -path "$Alpa`:\" -Include "*1*","*.lnk" -ErrorAction SilentlyContinue -Recurse -Force | Get-FileHash -Algorithm MD5 -ErrorAction SilentlyContinue
+			#Change Algorithm above from MD5 to another one if desired.
+			$Alp--}
+		Until ($Alp -lt 65)
 
-	ForEach ($hash in $hashs){    
-		#Place any hash\es below to match on. Either a single one "<HASH>" or multiple "((<HASH1>)|(<HASH2>)|(<HASH3>))"
-		if ($hash -match "((<HASH1>)|(<HASH2>))") {
-			$path = $hash | Select -Property path
-			#If you don't want to delete a file, change the below line to specify what you want the scrip to do with hash matched files.
-			Remove-Item -Path $path.Path -Force
-			Write-Host "Found a malicious hash on $target, deleted $($path.Path)"
+		ForEach ($hash in $hashs){    
+			#Place any hash\es below to match on. Either a single one "<HASH>" or multiple "((<HASH1>)|(<HASH2>)|(<HASH3>))"
+			if ($hash -match "((<HASH1>)|(<HASH2>))") {
+				$path = $hash | Select -Property path
+				#If you don't want to delete a file, change the below line to specify what you want the scrip to do with hash matched files.
+				Remove-Item -Path $path.Path -Force
+				Write-Host "Found a malicious hash on $target, deleted $($path.Path)"
+			}
 		}
-	}}
+	}
 }
 
 Remove-Item -Path "$home\hostlist.txt" -Force
